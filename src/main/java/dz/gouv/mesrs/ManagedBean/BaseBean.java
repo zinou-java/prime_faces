7a82ddaa5ae0c2dc7e5e7d13e6d6a6a07a90a384
Harchaoui, Ahmed <ahmed.harchaoui@sap.com>
2019-03-23 07:22:34 -0400

    add update and delete buttons. remove unused code.

--- src/main/java/dz/gouv/mesrs/ManagedBean/BaseBean.java
+++ src/main/java/dz/gouv/mesrs/ManagedBean/BaseBean.java
@@ -1,20 +1,12 @@
 package dz.gouv.mesrs.ManagedBean;
 
-import dz.gouv.mesrs.services.*;
+import dz.gouv.mesrs.services.EquipementService;
 import dz.gouv.mesrs.services.nc.NomenclatureService;
 import dz.gouv.mesrs.services.ppm.RefEtablissementService;
 import dz.gouv.mesrs.services.ppm.RefTypePieceConstitutiveService;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
-import java.io.IOException;
-import java.util.List;
 
 
 @Setter
